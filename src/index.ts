5e0728dc15db27c4363c995c02e724f82759ad4f
m-sohaibnadeem <sohaib@idenfo.com>
2024-09-18 19:54:26 +0500

    Typescript defintiion

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import Toast from "./components/Toast";
 import ToastContainer from "./components/ToastContainer";
 import {ToastContext,ToastProvider} from "./context/ToastContext"
 import useToast from "./hooks/useToast";
-import type {ToastOptions,ToastType,ToastContainerProps,ToastContextProps,ToastPosition,ToastProps,ToastProviderProps,ToastVariant,UseToastReturn} from "./types/types"
 export {Toast,ToastContainer,ToastContext,ToastProvider,useToast}
-export type {ToastOptions,ToastType,ToastContainerProps,ToastContextProps,ToastPosition,ToastProps,ToastProviderProps,ToastVariant,UseToastReturn as type}
+export * from './types/types';
